de78d976e39c8ccdc7d1c77669eb1498cc7e381e
Ayush Kumar Gautam <157630163+ayushgautamm@users.noreply.github.com>
2025-09-28 23:01:30 +0530

    Update CrimeRiskApiService.kt

--- CrimeRiskApiService.kt
+++ CrimeRiskApiService.kt
@@ -1,6 +1,7 @@
 package com.example.loginactivityshared.API
 
 
+
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -16,4 +17,5 @@ interface CrimeRiskApiService {
     suspend fun predictRisk(@Body request: PredictionRequest): PredictionResponse
 
 }
+
 
